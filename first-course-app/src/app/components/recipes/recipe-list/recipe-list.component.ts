6fb486f1d4df06c934700616848250f25a65661f
Szymon <regicsmaster@gmail.com>
2022-04-24 17:29:51 +0200

    Added Recipes model and list

--- first-course-app/src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ first-course-app/src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../../../models/recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('name', 'desc', 'https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg')
+  ];
 
   constructor() { }
 
