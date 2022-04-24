40a12fb1ec33f57c7ba400d42554d8e97fcae825
Szymon <regicsmaster@gmail.com>
2022-04-24 14:39:50 +0200

    Added String interpolation

--- angular-course/src/app/app.module.ts
+++ angular-course/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { FormsModule } from "@angular/forms";
 import { SuccesAlertComponent } from './components/succes-alert/succes-alert.component';
 import { WarningAlertComponent } from './components/warning-alert/warning-alert.component';
+import { ServerComponent } from './components/server/server.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SuccesAlertComponent,
     WarningAlertComponent,
+    ServerComponent,
   ],
   imports: [
     BrowserModule,
