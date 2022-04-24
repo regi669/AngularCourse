32db264af90f799d3fcfaffe6ab55bd862be4056
Szymon <regicsmaster@gmail.com>
2022-04-24 16:04:05 +0200

    ngif basic syntax

--- angular-course/src/app/components/servers/servers.component.ts
+++ angular-course/src/app/components/servers/servers.component.ts
@@ -9,6 +9,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
   serverName = '';
+  serverCreated = false;
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -19,7 +20,8 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created';
+    this.serverCreated = true;
+    this.serverCreationStatus = 'Server was created, name is ' + this.serverName;
   }
 
   // onUpdateServerName(event: Event) {
