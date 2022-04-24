c52125e0a94d1489c69c2dc926423f67212663a3
Szymon <regicsmaster@gmail.com>
2022-04-24 15:40:10 +0200

    Event Binding

--- angular-course/src/app/components/server/server.component.ts
+++ angular-course/src/app/components/server/server.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServerComponent implements OnInit {
   serverId = 10;
   status = 'offline';
+
   constructor() { }
 
   getServerStatus() {
