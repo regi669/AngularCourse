1a388ecc0bb5f2ec872e228a4328166ab557f4e9
Szymon <regicsmaster@gmail.com>
2022-04-26 11:03:19 +0200

    Recipe components can finally talk with each other :D

--- first-course-app/src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ first-course-app/src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from "../../../models/recipe.model";
 
 @Component({
@@ -7,14 +7,21 @@ import {Recipe} from "../../../models/recipe.model";
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipe:Recipe;
   recipes: Recipe[] = [
-    new Recipe('name', 'desc', 'https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg')
+    new Recipe('name', 'desc', 'https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg'),
+    new Recipe('name2', 'desc2', 'https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg')
   ];
+  @Output()
+  recipeSelectedList = new EventEmitter<Recipe>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe:Recipe) {
+    this.recipeSelectedList.emit(recipe)
   }
 
 }
