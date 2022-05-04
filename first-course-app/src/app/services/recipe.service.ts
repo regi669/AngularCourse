50b7f1df24684d401e29811338c6a4758a3547fe
Szymon <regicsmaster@gmail.com>
2022-05-04 14:26:16 +0200

    Implemented Ingredient showing in recipe

--- first-course-app/src/app/services/recipe.service.ts
+++ first-course-app/src/app/services/recipe.service.ts
@@ -1,5 +1,6 @@
 import {EventEmitter, Injectable} from '@angular/core';
 import { Recipe } from "../models/recipe.model";
+import {Ingredient} from "../models/ingredient.model";
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +10,20 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('name', 'desc', 'https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg'),
-    new Recipe('name2', 'desc2', 'https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg')
+    new Recipe('name',
+      'desc',
+      'https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Egg', 20)
+      ]),
+    new Recipe('name2',
+      'desc2',
+      'https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg',
+    [
+      new Ingredient('Meat', 25),
+      new Ingredient('Egg', 2)
+    ])
   ];
 
   getRecipes() {
